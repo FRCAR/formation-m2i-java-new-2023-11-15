52cdb6f1f97ed2766421239c383ed16f75ad8ada
François Caremoli <francois.caremoli@wanadoo.fr>
2023-11-14 10:40:52 +0100

    Premier commmit

---
+++ data-repository/src/main/java/com/bigcorp/project/data/repository/AddressServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bigcorp.project.data.repository;
+
+import com.bigcorp.project.data.contract.AddressService;
+
+/**
+ * Gère les opérations sur les User
+ */
+public class AddressServiceImpl implements AddressService   {
+
+	public String getPostCode(String address) {
+		return "44500";
+	}
+	
+}
